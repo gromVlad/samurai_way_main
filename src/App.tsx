2fcb8687ab874a4d439759144d71dff4b35c9f50
gromVlad <client7289@gmail.com>
2023-03-26 22:41:51 +0300

    add star

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,18 @@ function App() {
   return (
     <div className="App">
      Hello, samurai! Let's go! my name is Vlad
+     <Star />
+     <Star />
+     <Star />
+     <Star />
+     <Star />
     </div>
   );
 }
 
 export default App;
+
+function Star (){
+  debugger
+  return <div>*</div>
+}
