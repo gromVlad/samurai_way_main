15866c1197f3e338b3b6deaf70a1fc232e899884
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-03-29 22:04:33 +0300

    add css+h.n.m

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,41 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go! my name is Vlad
-     <Star />
-     <Star />
-     <Star />
-     <Star />
-     <Star />
-     <Text />
+      <header className="header">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Circle-icons-gamecontroller.svg/512px-Circle-icons-gamecontroller.svg.png"
+          alt="icons"
+        />
+      </header>
+      <nav className="nav">
+        <div>Profile</div>
+        <div>Message</div>
+        <div>News</div>
+        <div>Music</div>
+        <div>Settings</div>
+      </nav>
+      <main className="content">
+        <img
+          src="https://public.bnbstatic.com/image/cms/blog/20220329/7c0fb91f-61db-4541-9a33-8b76dde47b36.png"
+          alt="play"
+        />
+        <div className="ava">
+          <img
+            src="https://cspromogame.ru//storage/upload_images/avatars/3981.jpg"
+            alt="ava"
+          />
+        </div>
+        <p>description</p>
+        <div>My post</div>
+        <div>New post</div>
+        <div>post one</div>
+        <div>post two</div>
+      </main>
     </div>
   );
 }
 
 export default App;
-
-function Star (){
-  debugger
-  return <div>*</div>
-}
-
-//add fragment
-function Text (){
-  return <>usually text</>; //<-----fragment
-}
