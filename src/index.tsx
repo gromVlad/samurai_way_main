7fb751e691a5aa5dbadd54c396cc24cd6b262723
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-05-13 19:39:08 +0300

    95

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { store } from "./components/redusers/redux-store";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import {  ContainerApp } from "./App";
+import { HashRouter } from "react-router-dom";
+import { ContainerApp } from "./App";
 
 export const rerender = () => {
   ReactDOM.render(
-    <BrowserRouter>
-    <Provider store={store}>
-      <ContainerApp />
-    </Provider>
-    </BrowserRouter>,
+    <HashRouter>
+      <Provider store={store}>
+        <ContainerApp />
+      </Provider>
+    </HashRouter>,
     document.getElementById("root")
   );
 };
